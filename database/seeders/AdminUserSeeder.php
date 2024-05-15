2a060826719b7d82268c281c93c3dfa7b5f7b350
Rafal Zado <rafal.zado92@gmail.com>
2024-05-15 15:34:33 +0200

    admin seed

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => 'admin@example.com',
+            'email_verified_at' => now(),
+            'over_mij' => 'Ik ben de beheerder van de site.',
+            'profile_picture' => 'default_admin.png',
+            'is_admin' => true,
+            'password' => Hash::make('Rafal123'),
+            'remember_token' => Str::random(10),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
